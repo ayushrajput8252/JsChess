86c6c1329f2ca313d8093b64eaafe0d276afb6cc
Your Name <you@example.com>
2025-12-21 10:49:51 +0530

    leaning oops

--- helper2.py
+++ helper2.py
@@ -1,3 +1,17 @@
-a = "vhkjkl;l;gi"
+#CLASSS
+class employee:
+    #magic method
+    def __init__(self):
+        print("attributes are being initialized")
+        self.name = "John Jones"
+        self.salary = 1000
+        self.titel = "developer"
+        print("attributes initialized")
 
-print(a)
+    def travel(self, destination):
+        print("travel method is called")
+        print(f"{self.name} is going to {destination}")
+#OBJECT
+sam = employee()
+#METHOD CALL
+sam.travel("London")
